1f653af582b402ed859886dcb7d3626472dc00aa
Alexguld1234 <s224228@student.dtu.dk>
2024-04-10 13:45:31 +0200

    Merge branch 'main' of https://github.com/slayervictor/Fagpakkeprojekt-Eyes-Modelling

--- eye_tracking_expiriment/experiment.py
+++ eye_tracking_expiriment/experiment.py
@@ -5,9 +5,16 @@ import Licenses.licensefile as licensefile
 license_file = licensefile.license_file
 existing_files = sum(1 for file in os.listdir('eye_tracking_expiriment\data') if file.startswith('gaze_data'))
 
-
-testPerson = 
-testPersonName = "Filip"
+# Vindue lukket
+# Gardiner for
+# fuld lysstyrke på pc
+# kalibreret eyetracker
+testPerson = 7
+testPersonName = "Johan"
+# Efter experiment, hvis fil stor. Lav det til en zip fil.
+# Tilføj csv filen i gitignore.
+testPerson = 8
+testPersonName = "Bengisu"
 filename = f'eye_tracking_expiriment\data\gaze_data_{testPersonName}_{testPerson}.csv'
 
 # from psychopy import prefs, visual, core, event, monitors, tools, logging
